c31ad3872c566b627c44923555734ba2cc76e438
Daniel Escobar <escobard90@gmail.com>
2017-12-11 23:32:27 -0700

    style: updated header text styles, and syntax structure

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -7,11 +7,11 @@ export default class Header extends Component {
   render() {
     return (
       <section id="header" className="col-md-12 col-sm-12">
-      	<div className="col-md-3 col-sm-12 header-image">
-      		<img src="" alt=""/>
+      	<div className="col-md-3 col-sm-12 header-title">
+      		UdaciMap
       	</div>
       	<div className="col-md-9 col-sm-12 header-links">
-      		<a href="">About</a>
+      		<a href="#github">About</a>
       	</div>
       </section>
     );
