66a162196e92fe1a204aa7667b14014508113703
Daniel Escobar <escobard90@gmail.com>
2017-12-11 21:01:13 -0700

    feat: updated webpack, added sass dependency, can now add invidual sass files on a component basis

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,7 @@
 v0.1
+    - Completed:
+        - Add bootstrap.min stylesheet for rapid prototyping.
     - TODO:
         - Build a bare-bones interface using the react-redux boilerplate by Stephen Grider.
-        - Add bootstrap.min stylesheet for rapid prototyping. 
+        - Add sass styles on a component level basis
+         
---
+++ src/components/Header/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+export default class Header extends Component {
+  
+  render() {
+    return (
+      <section id="header" className="col-md-12 col-sm-12">
+      	<div className="col-md-3 col-sm-12 header-image">
+      		<img src="" alt=""/>
+      	</div>
+      	<div className="col-md-9 col-sm-12 header-links">
+      		
+      	</div>
+      </section>
+    );
+  }
+}
