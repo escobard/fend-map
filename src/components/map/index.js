c73e6ba9ade0070306e2695558fc67d1619fe48e
Daniel Escobar <escobard90@gmail.com>
2017-12-13 23:31:30 -0700

    feat: added a base google map, and map pins on click

--- src/components/map/index.js
+++ src/components/map/index.js
@@ -5,18 +5,33 @@ import styles from './styles.scss'
 class Map extends Component {
   componentDidMount () {
     this.map();
+
+    this.map = this.map.bind(this);
+    this.placeMarker = this.placeMarker.bind(this);
   }
 
   map() {
-    return new window.google.maps.Map(document.getElementById('map'), {
+  	let placeMarker = this.placeMarker
+    let map = new window.google.maps.Map(document.getElementById('map'), {
       center: {
         lat: parseFloat(51.507351),
         lng: parseFloat(-0.127758)
       },
       zoom: 10
     })
+    map.addListener('click', function(e) {
+	    placeMarker(e.latLng, map);
+	});
   }
 
+  placeMarker(location, map){
+  	console.log(location, map)
+    let marker = new google.maps.Marker({
+        position: location, 
+        map: map
+    });
+    map.panTo(location); 	
+  }
   render() {
     return (
       <div>
