2c029ba4440db98a772392de6880727e7b1e02b0
Daniel Escobar <escobard90@gmail.com>
2017-12-14 18:19:53 -0700

    feat: added redux to listing container

--- src/containers/listing/index.js
+++ src/containers/listing/index.js
@@ -1,14 +1,32 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
+
+import ListItem from '../../components/listItem'
 
 import styles from './styles.scss'
 
-export default class Listing extends Component {
-  
+class Listing extends Component {
+
+  renderListItems(listItems){
+  	return listItems.map((item, index) => {
+	  		return (
+	  			<ListItem item={item} key={index}/>
+	  		)
+  		}
+  	)
+  }
+
   render() {
     return (
       <main id="listing" className="container">
-        <p>This is where the listings view will go</p>
+        {this.renderListItems(this.props.markers)}
       </main>
     );
   }
 }
+
+function mapStateToProps({markers}){
+	return {markers}
+}
+
+export default connect(mapStateToProps)(Listing)
